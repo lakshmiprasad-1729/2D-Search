3e3bd1ad51e4dabb52ff834fd555e79365404b55
shiva9478 <shivam69911@gmail.com>
2024-11-16 13:40:22 +0530

    hi

--- Assets/Scripts/ChangeColorsOnInstance.cs
+++ Assets/Scripts/ChangeColorsOnInstance.cs
@@ -13,4 +13,5 @@ public class ChangeColorsOnInstance : MonoBehaviour
             spriteRenderer.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
         }
     }
+    
 }
